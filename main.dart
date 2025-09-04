293c34a53116bb19fbe5f9e9254759be9023cfb8
G4br1el01 <gabrielpolicarpo@uni9.edu.br>
2025-09-04 00:00:10 +0000

    feat: if statement

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
-void main (){
-print('olá mundo!');
+import 'dart:io';
+void main(){
+    int idade = 18;
+    if(idade >=18)
+{
+    print('Pode entrar na balada');
+}
+    print('Fim do programa');
+
 }
