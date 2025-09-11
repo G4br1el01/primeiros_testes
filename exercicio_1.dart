f87cc8e73f5b2313c53e048f3a7ca62cb9d0017c
G4br1el01 <gabrielpolicarpo@uni9.edu.br>
2025-09-11 00:27:55 +0000

    feat: if statement

---
+++ exercicio_1.dart
@@ -0,0 +1,9 @@
+void main() {
+  int idade = 18;
+
+  if (idade < 16) {
+    print('Não pode votar');
+  } else {
+    print('Pode votar');
+  }
+}
--- main.dart
+++ main.dart
@@ -1,5 +1,13 @@
 import 'dart:io';
 void main(){
 
+if(>1400){
+    print('Varejo');}
+    else(>5000){
+        print('vangogh');
+    }
+
 
 }
+
+
