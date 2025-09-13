858170b4ce12b9f7f8c100c12e64523519e48e28
G4br1el01 <gabrielpolicarpo@uni9.edu.br>
2025-09-13 22:27:16 +0000

    feat: if statment

--- exercicio_3.dart
+++ exercicio_3.dart
@@ -1,36 +1,28 @@
 import 'dart:io';
+
 void main() {
+  // peso
+  stdout.write('Digite seu peso: ');
+  double peso = double.parse(stdin.readLineSync()!);
 
-//formula do imc
-    double imc = peso / (altura*altura);
-    print('Seu IMC é: ${imc.toStringAsFixed(2)}');
+  // altura
+  stdout.write('Digite sua altura: ');
+  double altura = double.parse(stdin.readLineSync()!);
 
-//peso
-stdout.write('Digite seu peso: ');
-double peso = double.parse(stdin.readLineSync()!);
+  // cálculo do IMC
+  double imc = peso / (altura * altura);
+  print('Seu IMC é: ${imc.toStringAsFixed(2)}');
 
-//altura
-stdout.write('Digite sua altura: ');
-double altura = double.parse(stdin.readLineSync()!);
-
-//menssagem do peso
-if (imc < 18.5){
-print('Abaixo do peso');}
-
-else if (imc < 24.9){
+  // classificação do IMC
+  if (imc < 18.5) {
+    print('Abaixo do peso');
+  } else if (imc < 24.9) {
     print('Peso normal');
-}
-
-else if (imc < 34.9){
-    print('Obsidade grau 1');
-}
-
-else if (imc < 39.9){
-    print('Obsidade grau 2 ');
-}
-
-else (imc > 40.0){
-    print('Obsidade grau 3');
-}
-
+  } else if (imc < 34.9) {
+    print('Obesidade grau 1');
+  } else if (imc < 39.9) {
+    print('Obesidade grau 2');
+  } else {
+    print('Obesidade grau 3');
+  }
 }
